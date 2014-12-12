2ad7d1ef2b42ca9bf5c9fbe16434ef53a94cda49
ShtankoAleksey <aleksey.shtanko@yandex.ru>
2014-12-12 19:22:48 +0200

    Add class ShowData with fragments

--- Vstat/app/src/main/java/shtanko/com/vstat/Main.java
+++ Vstat/app/src/main/java/shtanko/com/vstat/Main.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class Main extends ActionBarActivity {
@@ -17,23 +18,17 @@ public class Main extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
+
+        menu.add("1");
+        menu.add("2");
+
+       return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
     }
 }
