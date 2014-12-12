de3174bc52a12e3fabac79a721e68523d95fd2e1
ShtankoAleksey <aleksey.shtanko@yandex.ru>
2014-12-12 20:12:50 +0200

    Add Json Parser

--- Vstat/app/src/main/java/shtanko/com/vstat/ShowData.java
+++ Vstat/app/src/main/java/shtanko/com/vstat/ShowData.java
@@ -1,5 +1,6 @@
 package shtanko.com.vstat;
 
+import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -11,9 +12,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class ShowData extends ActionBarActivity {
+
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,8 @@ public class ShowData extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        name = (TextView)findViewById(R.id.channel_name);
     }
 
 
@@ -58,7 +74,7 @@ public class ShowData extends ActionBarActivity {
         }
     }
 
-    //
+    //Class for name channel
     public static class DescriptionFragment extends Fragment {
 
 
@@ -72,6 +88,97 @@ public class ShowData extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_description, container, false);
             return rootView;
         }
+    }
+    //Class Json Parser
+    private class ParseTask extends AsyncTask<Void, Void, String> {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String resultJson = "";
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // получаем данные с внешнего ресурса
+            try {
+                String str_url = "http://gdata.youtube.com/feeds/api/users/" + data + "?alt=json";
+                URL url = new URL(str_url);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                resultJson = buffer.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultJson;
+        }
+
+        @Override
+        protected void onPostExecute(String strJson) {
+            super.onPostExecute(strJson);
+            // выводим целиком полученную json-строку
+            //Log.d(LOG_TAG, strJson);
+            JSONObject dataJsonObj;
+            String $t;
+            String name;
+            String location;
+            String subscriberCount;
+            String totalUploadViews;
+            String photo;
+
+            try {
+                dataJsonObj = new JSONObject(strJson);
+                JSONObject entryJSONObject = dataJsonObj.getJSONObject("entry");
+
+                JSONArray authorArray = entryJSONObject.getJSONArray("author");
+                JSONObject authorJSONObject = authorArray.getJSONObject(0);
+                JSONObject nameJSONObject = authorJSONObject.getJSONObject("name");
+                name = nameJSONObject.getString("$t");
+                // Log.d(LOG_TAG, "Name: " + name);
+
+                JSONObject contentJSONObject2 = entryJSONObject.getJSONObject("yt$location");
+                location = contentJSONObject2.getString("$t");
+                //Log.d(LOG_TAG, "Location: " + location);
+
+                JSONObject contentJSONObject3 = entryJSONObject.getJSONObject("yt$statistics");
+                subscriberCount = contentJSONObject3.getString("subscriberCount");
+                totalUploadViews = contentJSONObject3.getString("totalUploadViews");
+                // Log.d(LOG_TAG, "Subscriber Count: " + subscriberCount);
+                // Log.d(LOG_TAG, "total Upload Views: " + totalUploadViews);
+
+                JSONObject contentJSONObject = entryJSONObject.getJSONObject("content");
+                $t = contentJSONObject.getString("$t");
+                //Log.d(LOG_TAG, "Scheme: " + $t);
+
+                JSONObject photoJSONObject = entryJSONObject.getJSONObject("media$thumbnail");
+                photo = photoJSONObject.getString("url");
+                //Log.d(LOG_TAG, "Photo: " + photo);
+
+//                tw.setText($t);
+//                tw2.setText(name);
+//                tw3.setText(location);
+//                tw4.setText(subscriberCount);
+//                tw5.setText(totalUploadViews);
+//                tw6.setText(photo);
+
+/*
+                number_of_subscribers.setText(subscriberCount);
+                number_of_views.setText(totalUploadViews);
+                //sign_up.setText();
+                name_data.setText(name);
+                */
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 
