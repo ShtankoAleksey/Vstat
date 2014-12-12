b5a728bbc4679f149029ae4ebedc6ceb604561d2
ShtankoAleksey <aleksey.shtanko@yandex.ru>
2014-12-12 19:54:19 +0200

    Delete circle imageview class

--- Vstat/app/src/main/java/shtanko/com/vstat/Main.java
+++ Vstat/app/src/main/java/shtanko/com/vstat/Main.java
@@ -1,18 +1,26 @@
 package shtanko.com.vstat;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 
-public class Main extends ActionBarActivity {
+public class Main extends ActionBarActivity implements OnClickListener {
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        go = (Button) findViewById(R.id.go);
+        go.setOnClickListener(this);
     }
 
 
@@ -22,7 +30,7 @@ public class Main extends ActionBarActivity {
         menu.add("1");
         menu.add("2");
 
-       return super.onCreateOptionsMenu(menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -30,5 +38,18 @@ public class Main extends ActionBarActivity {
 
         Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.go:
+// TODO Call second activity
+                Intent intent = new Intent(this, ShowData.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
